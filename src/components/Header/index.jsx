8baf81f840ddff2cbdd78f0982a76c8549699b94
Шмалюк Дмитро <shmalyk9000@gmail.com>
2019-10-20 21:21:19 +0300

    cleared the project and created a new component (Header)

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './style.css';
+
+function Header (){
+    return(
+        <header>
+            <section>
+                <div className="title">
+                    <span>Timetable</span>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li>Головна</li>
+                        <li>Розклад на сьогодні</li>
+                        <li>Розклад на завтра</li>
+                    </ul>
+                </nav>
+
+                <div className = "authorization">
+                    Увійти
+                </div>
+            </section>
+        </header>
+    );
+}
+
+export default Header;
