da69cf9ddafc7e281440b82b02e0f3f74e5b5587
Dmytro Shmaliuk <dimkas2001@icloud.com>
2019-11-30 04:09:02 +0200

    refactor (MainTimetable/index,jsx, MainTimetable/style.css): add new section to table cell

--- src/components/MainTimetable/index.jsx
+++ src/components/MainTimetable/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { Table, Button} from 'antd';
 import 'antd/dist/antd.css';
 import "./style.css";
@@ -8,37 +8,43 @@ const columns=[
         title: 'П-21',
         dataIndex: 'p21',
         key: 'p21',
-        render: text => <p>{text}</p>,
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
     },
     {
         title: 'П-22',
         dataIndex: 'p22',
         key: 'p22',
-        render: text => <p>{text}</p>,
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
     },
     {
         title: 'П-31',
         dataIndex: 'p31',
         key: 'p31',
-        render: text => <p>{text}</p>,
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
     },
     {
         title: 'П-32',
         dataIndex: 'p32',
         key: 'p32',
-        render: text => <p>{text}</p>,
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
     },
     {
         title: 'П-41',
         dataIndex: 'p41',
         key: 'p41',
-        render: text => <p>{text}</p>,
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
     },
     {
         title: 'П-42',
         dataIndex: 'p42',
         key: 'p42',
-        render: text => <p>{text}</p>,
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
     },
 
 ];
@@ -46,40 +52,49 @@ const columns=[
 const data = [
     {
       key: '1',
-      p21: 'ТОЕ',
-      p22: 'Вступ до фаху',
-      p31: '',
-      p32: 'Математична логіка',
-      p41: 'Економіка IT',
-      p42: 'ММДО'
+      p21: {name: 'ТОЕ', lectureHall: '310', teacher: 'Антипчук Б.О.'},
+      p22: {name: 'Вступ до фаху', lectureHall: '101', teacher: 'Данильченко'},
+      p31: {name: '', lectureHall: '', teacher: ''},
+      p32: {name: 'Математична логіка', lectureHall: '16', teacher: 'Пустовіт Л.В.'},
+      p41: {name: 'Економіка IT', lectureHall: '419а', teacher: 'Габрійчук Н.І.'},
+      p42: {name: 'ММДО', lectureHall: '21', teacher: 'Можаровський С.В.'}
     },
     {
         key: '2',
-        p21: 'Іноземна мова',
-        p22: 'Вища математика',
-        p31: 'Математична логіка',
-        p32: 'ТСПП',
-        p41: 'ММДО',
-        p42: 'Економіка IT'
+        p21: {name: 'Іноземна мова', lectureHall: '4', teacher: 'Коновалова В.А.'},
+        p22: {name: 'Вища математика', lectureHall: '15', teacher: 'Головач О.П.'},
+        p31:  {name: 'Математична логіка', lectureHall: '16', teacher: 'Пустовіт Л.В.'},
+        p32: {name: 'ТСПП', lectureHall: '18', teacher: 'Устименко Я.І'},
+        p41: {name: 'ММДО', lectureHall: '21', teacher: 'Можаровський С.В.'},
+        p42: {name: 'Економіка IT', lectureHall: '419а', teacher: 'Габрійчук Н.І.'}
     },
     {
         key: '3',
-        p21: 'Вступ до фаху',
-        p22: 'Фіз.вих',
-        p31: 'ТСПП',
-        p32: 'Чисельні методи',
-        p41: 'Фіз.вих',
-        p42: ''
+        p21: {name: 'Вступ до фаху', lectureHall: '101', teacher: 'Данильченко'},
+        p22: {name: 'Фізичне виховання', lectureHall: '', teacher: 'Єль'},
+        p31: {name: 'ТСПП', lectureHall: '18', teacher: 'Устименко Я.І'},
+        p32: {name: 'Чисельні методи', lectureHall: '21', teacher: 'Можаровський С.В.'},
+        p41: {name: 'Фізичне виховання', lectureHall: '', teacher: 'Свірідов'},
+        p42: {name: '', lectureHall: '', teacher: ''}
     },
     {
         key: '4',
-        p21: 'Укр.мова (факультатив)',
-        p22: 'Укр.мова (факультатив)',
-        p31: '',
-        p32: '',
-        p41: '',
-        p42: ''
+        p21: {name: 'Укр.мова (факультатив)', lectureHall: '420', teacher: 'Семенюк В.М.'},
+        p22: {name: 'Укр.мова (факультатив)', lectureHall: '305', teacher: 'Емець Л.В.'},
+        p31: {name: 'Чисельні методи', lectureHall: '21', teacher: 'Можаровський С.В.'},
+        p32: {name: '', lectureHall: '', teacher: ''},
+        p41: {name: '', lectureHall: '', teacher: ''},
+        p42: {name: '', lectureHall: '', teacher: ''}
     },
+    {
+        key: '5',
+        p21: {name: '', lectureHall: '', teacher: ''},
+        p22: {name: '', lectureHall: '', teacher: ''},
+        p31: {name: '', lectureHall: '', teacher: ''},
+        p32: {name: '', lectureHall: '', teacher: ''},
+        p41: {name: '', lectureHall: '', teacher: ''},
+        p42: {name: '', lectureHall: '', teacher: ''}
+    }
 ];
 
 function MainTimetable(){
