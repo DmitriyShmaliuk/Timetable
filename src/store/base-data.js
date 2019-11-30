7b6c3cf2eb32b1e088b35c487ae77fb51e5778c3
Dmytro Shmaliuk <dimkas2001@icloud.com>
2019-11-30 05:27:29 +0200

    add (base-data.js): base data for the empty table

---
+++ src/store/base-data.js
@@ -0,0 +1,39 @@
+import React, {Fragment} from 'react';
+
+export default {
+    columns: [{
+        title: "П-21",
+        dataIndex: "p21",
+        key: "p21",
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
+    },
+    {
+        title: "П-21",
+        dataIndex: "p21",
+        key: "p21",
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
+    },
+    {
+        title: "П-21",
+        dataIndex: "p21",
+        key: "p21",
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
+    },
+    {
+        title: "П-21",
+        dataIndex: "p21",
+        key: "p21",
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
+    },
+    {
+        title: "П-21",
+        dataIndex: "p21",
+        key: "p21",
+        render: text => <Fragment><p>{text.name}</p><span>{text.teacher}</span>
+            <span className = "lecture-hall">{text.lectureHall}</span></Fragment>,
+    }]
+}
