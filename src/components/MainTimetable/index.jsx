a60d3331e6844bd8068af332cfeae5a9bce539a2
Dmytro Shmaliuk <dimkas2001@icloud.com>
2019-12-01 00:42:58 +0200

    add (MainTimetable): data from redux store and refactor style

--- src/components/MainTimetable/index.jsx
+++ src/components/MainTimetable/index.jsx
@@ -1,66 +1,27 @@
 import React from 'react';
-import { Table, Button} from 'antd';
+import { Table, Button, Breadcrumb} from 'antd';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import 'antd/dist/antd.css';
 import "./style.css";
 
-const data = [
-    {
-      key: '1',
-      p21: {name: 'ТОЕ', lectureHall: '310', teacher: 'Антипчук Б.О.'},
-      p22: {name: 'Вступ до фаху', lectureHall: '101', teacher: 'Данильченко'},
-      p31: {name: '', lectureHall: '', teacher: ''},
-      p32: {name: 'Математична логіка', lectureHall: '16', teacher: 'Пустовіт Л.В.'},
-      p41: {name: 'Економіка IT', lectureHall: '419а', teacher: 'Габрійчук Н.І.'},
-      p42: {name: 'ММДО', lectureHall: '21', teacher: 'Можаровський С.В.'}
-    },
-    {
-        key: '2',
-        p21: {name: 'Іноземна мова', lectureHall: '4', teacher: 'Коновалова В.А.'},
-        p22: {name: 'Вища математика', lectureHall: '15', teacher: 'Головач О.П.'},
-        p31:  {name: 'Математична логіка', lectureHall: '16', teacher: 'Пустовіт Л.В.'},
-        p32: {name: 'ТСПП', lectureHall: '18', teacher: 'Устименко Я.І'},
-        p41: {name: 'ММДО', lectureHall: '21', teacher: 'Можаровський С.В.'},
-        p42: {name: 'Економіка IT', lectureHall: '419а', teacher: 'Габрійчук Н.І.'}
-    },
-    {
-        key: '3',
-        p21: {name: 'Вступ до фаху', lectureHall: '101', teacher: 'Данильченко'},
-        p22: {name: 'Фізичне виховання', lectureHall: '', teacher: 'Єль'},
-        p31: {name: 'ТСПП', lectureHall: '18', teacher: 'Устименко Я.І'},
-        p32: {name: 'Чисельні методи', lectureHall: '21', teacher: 'Можаровський С.В.'},
-        p41: {name: 'Фізичне виховання', lectureHall: '', teacher: 'Свірідов'},
-        p42: {name: '', lectureHall: '', teacher: ''}
-    },
-    {
-        key: '4',
-        p21: {name: 'Укр.мова (факультатив)', lectureHall: '420', teacher: 'Семенюк В.М.'},
-        p22: {name: 'Укр.мова (факультатив)', lectureHall: '305', teacher: 'Емець Л.В.'},
-        p31: {name: 'Чисельні методи', lectureHall: '21', teacher: 'Можаровський С.В.'},
-        p32: {name: '', lectureHall: '', teacher: ''},
-        p41: {name: '', lectureHall: '', teacher: ''},
-        p42: {name: '', lectureHall: '', teacher: ''}
-    },
-    {
-        key: '5',
-        p21: {name: '', lectureHall: '', teacher: ''},
-        p22: {name: '', lectureHall: '', teacher: ''},
-        p31: {name: '', lectureHall: '', teacher: ''},
-        p32: {name: '', lectureHall: '', teacher: ''},
-        p41: {name: '', lectureHall: '', teacher: ''},
-        p42: {name: '', lectureHall: '', teacher: ''}
-    }
-];
-
 function MainTimetable({tables}){
     return(
-        <div className="content"> 
+        <div className="content">
+            <Breadcrumb mode="horizontal">
+                <Breadcrumb.Item><Link>Агрономія</Link></Breadcrumb.Item>
+                <Breadcrumb.Item><Link>Економічне</Link></Breadcrumb.Item>
+                <Breadcrumb.Item><Link>Агроінженерія</Link></Breadcrumb.Item>
+                <Breadcrumb.Item><Link>Будівництво та цивільна інженерія</Link></Breadcrumb.Item>
+                <Breadcrumb.Item><Link>Електрифікація та інформаційні системи</Link></Breadcrumb.Item>
+            </Breadcrumb>
+            
             <div className="time-table">
-                <Table columns={tables[0].columns} dataSource = {data} pagination = {false} bordered/>
-                <Table columns={tables[1].columns} dataSource = {data} pagination = {false} bordered/>
-                <Table columns={tables[2].columns} dataSource = {data} pagination = {false} bordered/>
-                <Table columns={tables[3].columns} dataSource = {data} pagination = {false} bordered/>
-                <Table columns={tables[4].columns} dataSource = {data} pagination = {false} bordered/>
+                <Table columns={tables[0].columns} dataSource = {tables[0].data} pagination = {false} bordered/>
+                <Table columns={tables[1].columns} dataSource = {tables[1].data} pagination = {false} bordered/>
+                <Table columns={tables[2].columns} dataSource = {tables[2].data} pagination = {false} bordered/>
+                <Table columns={tables[3].columns} dataSource = {tables[3].data} pagination = {false} bordered/>
+                <Table columns={tables[4].columns} dataSource = {tables[4].data} pagination = {false} bordered/>
             </div>
 
             <div id="changeBtn">
