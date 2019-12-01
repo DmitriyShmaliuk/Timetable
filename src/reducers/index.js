3cddb234e47d1f0b56ac6d47c2443f6f2cb20510
Dmytro Shmaliuk <dimkas2001@icloud.com>
2019-12-02 00:12:35 +0200

    add (reducers) two new reducers for users

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import {combineReducers} from 'redux';
 import tables from './tables';
+import users from './users';
+import user from './user';
 
-const rootReducer = combineReducers({tables});
+const rootReducer = combineReducers({tables,users,user});
 export default rootReducer;
