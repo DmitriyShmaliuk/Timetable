70a2d76683958535914d03dbde3f94dbb37ffe58
Dmytro Shmaliuk <dimkas2001@icloud.com>
2019-12-02 01:42:21 +0200

    add (actionCreator.js): action for setting current user

--- src/actions/actionCreator.js
+++ src/actions/actionCreator.js
@@ -1,4 +1,4 @@
-import {CHANGE_TIMETABLE} from '../constants';
+import {CHANGE_TIMETABLE, SET_USER} from '../constants';
 
 export const changeTimetable = (day, discipline, group, name, teacher, audience, speciality)=>({
     type:CHANGE_TIMETABLE,
@@ -9,4 +9,12 @@ export const changeTimetable = (day, discipline, group, name, teacher, audience,
     teacher,
     audience,
     speciality
+});
+
+export const setCurrentUser = (typeName, login, name, password) =>({
+    type: SET_USER,
+    typeName,
+    login,
+    name,
+    password
 });
