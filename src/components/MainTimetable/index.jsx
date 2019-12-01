3247c8f6ad8262b764c6f43630aa2cae44e67e03
Dmytro Shmaliuk <dimkas2001@icloud.com>
2019-12-01 16:33:19 +0200

    add (MainTimetable): change form and functions for submit data

--- src/components/MainTimetable/index.jsx
+++ src/components/MainTimetable/index.jsx
@@ -1,12 +1,98 @@
 import React, {useState} from 'react';
-import { Table, Button, Breadcrumb} from 'antd';
+import { Table, Button, Breadcrumb, Select, Input, Icon} from 'antd';
 import {connect} from 'react-redux';
-import ChangeForm from '../ChangeForm/index';
 import 'antd/dist/antd.css';
 import "./style.css";
 
+const {Option} = Select;
+
+const day = [{
+    key: "monday",
+    value: "Понеділок",
+},
+{
+    key: "tuesday",
+    value: "Вівторок",
+},
+{
+    key: "wedndesday",
+    value: "Середа",
+},
+{
+    key: "thursday",
+    value: "Четвер",
+},
+{
+    key: "friday",
+    value: "П'ятниця",
+}];
+
+const disciplines =[{
+    key: "first",
+    value: "Перша",
+},
+{
+    key: "second",
+    value: "Друга",
+},
+{
+    key: "third",
+    value: "Третя",
+},
+{
+    key: "fourth",
+    value: "Четверта",
+},
+{
+    key: "fifth",
+    value: "П'ята",
+}];
+
 function MainTimetable({tables}){
     const [tableIndex, setTableIndex] = useState(0);
+    const [isDisplayForm, setIsDisplayForm] = useState(false);
+    const [data, setData] = useState({day: '', group: '', disciplines: '',
+        name: '', teacher: '', audience: ''});
+
+    const changeTimetable = function (){
+    }
+
+    const handlerChangeDay = function (value){
+        const newData = data;
+        newData.day = value;
+        setData(newData);
+    }
+
+    const handlerChangeGroup = function (value){
+        const newData = data;
+        newData.group = value;
+        setData(newData);
+    }
+
+    const handlerChangeDisciplines = function (value){
+        const newData = data;
+        newData.disciplines = value;
+        setData(newData);
+    }
+
+    const handlerChangeName = function (event){
+        const newData = data;
+        newData.name = event.target.value;
+        setData(newData);
+    }
+
+    const handlerChangeTeacher = function (event){
+        const newData = data;
+        newData.teacher = event.target.value;
+        setData(newData);
+    }
+
+    const handlerChangeAudience = function (event){
+        const newData = data;
+        newData.audience = event.target.value;
+        setData(newData);
+    }
+
     return(
         <div className="content">
             <Breadcrumb mode="horizontal">
@@ -26,10 +112,44 @@ function MainTimetable({tables}){
             </div>
 
             <div id="changeBtn">
-                <Button id="changeBtn" size="large" type="danger" ghost>Змінити розклад</Button>
+                <Button id="changeBtn" size="large" type="danger" ghost onClick={()=>setIsDisplayForm(true)}>Змінити розклад</Button>
             </div>
 
-            <ChangeForm groups={tables[tableIndex].columns}/>
+            <form className="change-form" style={{display: isDisplayForm?"block":"none"}}>
+                <header>
+                    <h2>Змінити розклад</h2>
+                    <Icon id="closeIcon" type="close" onClick={()=>setIsDisplayForm(false)}/>
+                </header>
+
+                <section id="mainData">
+                    <Select id="day" placeholder="Оберіть день" size="large" onChange={handlerChangeDay}>
+                        {day.map(el => <Option value={el.key} key={el.key}>{el.value}</Option>)}
+                    </Select>
+
+                    <Select id="group" placeholder="Оберіть групу" size="large" onChange={handlerChangeGroup}>
+                        {tables[tableIndex].columns.map(el => <Option value={el.key} key={el.key}>{el.title}</Option>)}
+                    </Select>
+
+                    <Select id="disciplines" placeholder="Оберіть пару" size="large" onChange={handlerChangeDisciplines}>
+                        {disciplines.map(el => <Option value={el.key} key={el.key}>{el.value}</Option>)}
+                    </Select>
+                </section>
+
+                <section>
+                    <header>
+                        <h3>Інформація про пару</h3>
+                    </header>
+
+                    <Input name="name" placeholder="Назва предмету" size="large" onChange={handlerChangeName}/>
+                    <Input name="teacher" placeholder="Ім'я викладача" size="large" onChange={handlerChangeTeacher}/>
+                    <Input name="audience" placeholder="Номер аудиторії" size="large" onChange={handlerChangeAudience}/>
+                </section>
+
+                <section id="submitBtn">
+                    <Button type="primary" size="large" style={{width: '125px'}}ghost
+                        onClick={changeTimetable}>Змінити</Button>
+                </section>
+            </form>
         </div>
     );
 }
